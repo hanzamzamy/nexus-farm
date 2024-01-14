ed490a5f37fdacd8cb51e34dfc00b8e508c14d09
rayha <rayhanzamzamy.portable@gmail.com>
2024-01-14 22:08:18 +0700

    add another screen, EntryPage

--- app/src/main/java/id/nexus/farmap/ui/Navigator.kt
+++ app/src/main/java/id/nexus/farmap/ui/Navigator.kt
@@ -3,5 +3,6 @@ package id.nexus.farmap.ui
 sealed class Navigator(val route: String) {
     object MapLoad: Navigator("MapLoad")
     object MainMenu: Navigator("MainMenu")
+    object EntryPage: Navigator("EntryPage")
     object ARNav: Navigator("ARNav")
 }
