c7b114926016fc5934349772762b047c36c11b8a
rayha <rayhanzamzamy.portable@gmail.com>
2024-01-16 14:32:33 +0700

    EntryPage AdminMode is now supported

--- app/src/main/java/id/nexus/farmap/ui/Composables.kt
+++ app/src/main/java/id/nexus/farmap/ui/Composables.kt
@@ -6,9 +6,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.material.icons.filled.Place
-import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -330,88 +328,116 @@ fun EntryPage(
         contentAlignment = Alignment.TopCenter
     ) {
         Column{
-//            if(MainUI.adminMode) {
-//                var editor by remember { mutableStateOf(false) }
-//                var editDesc by remember { mutableStateOf(MainUI.map.mapNodes[label]?.get("longDesc") as String) }
-//                var descError by remember { mutableStateOf(false) }
-//
-//                Box(
-//                    modifier = Modifier.fillMaxWidth(),
-//                    contentAlignment = Alignment.BottomEnd
-//                ){
-//                    LazyRow {
-//                        items(5/*MainUI.map.mapNodes[label]?.get("imagesUrl") as List<String>*/){url->
-//                            AsyncImage(
-//                                model = "https://ipsf.net/wp-content/uploads/2021/12/dummy-image-square.webp",//url,
-//                                contentDescription = null,
-//                                contentScale = ContentScale.FillHeight,
-//                                modifier = Modifier.fillMaxHeight(0.5f)
-//                            )
-//                        }
-//                    }
-//
-//                    FloatingActionButton(
-//                        modifier = Modifier.padding(24.dp),
-//                        onClick = {
-//                            navController.navigate(Navigator.ARNav.route)
-//                            scope.launch {
-//                                snackbarHostState.showSnackbar("[INFO] Routing to $label")
-//                            }
-//                        }
-//                    ){
-//                        Icon(Icons.Filled.Send, null)
-//                    }
-//                }
-//
-//                Column(
-//                    horizontalAlignment = Alignment.Start,
-//                    verticalArrangement = Arrangement.spacedBy(16.dp),
-//                    modifier = Modifier.padding(16.dp)
-//                ){
-//                    Text("A123" /*label*/, fontSize = 24.sp)
-//                    (MainUI.map.mapNodes[label]?.get("longDesc") as? String)?.let { Text(it) }
-//
-//                    TextField(
-//                        value = editDesc,
-//                        onValueChange = {
-//                            descError = it.isEmpty()
-//                            editDesc = it
-//                        },
-//                        label = { Text("Description") },
-//                        singleLine = false,
-//                        isError = descError,
-//                        modifier = Modifier.fillMaxWidth()
-//                    )
-//                }
-//
-//                Column(
-//                    horizontalAlignment = Alignment.CenterHorizontally,
-//                    verticalArrangement = Arrangement.Bottom
-//                ){
-//                    Button(
-//                        modifier = Modifier.fillMaxWidth(),
-//                        onClick = {
-//                            if (editor) {
-//                                if (!descError) {
-//                                    //TODO
-//                                    editor = false
-//                                }
-//                            } else{
-//                                editor = true
-//                            }
-//                        }) {
-//                        Text(if (editor) "Save" else "Edit")
-//                    }
-//                }
-//            }else{
+            if (MainUI.adminMode) {
+                var editor by remember { mutableStateOf(false) }
+                var editDesc by remember { mutableStateOf(MainUI.map.mapNodes[label]?.get("longDesc") as String) }
+                var descError by remember { mutableStateOf(false) }
+                var editUrl by remember {
+                    mutableStateOf(MainUI.map.mapNodes[label]?.get("imagesUrl").let {
+                        it as List<String>
+                        it.joinToString("\n")
+                    })
+                }
+                var urlError by remember { mutableStateOf(false) }
+
                 Box(
                     modifier = Modifier.fillMaxWidth(),
                     contentAlignment = Alignment.BottomEnd
-                ){
+                ) {
                     LazyRow {
-                        items(5/*MainUI.map.mapNodes[label]?.get("imagesUrl") as List<String>*/){url->
+                        items(MainUI.map.mapNodes[label]?.get("imagesUrl") as List<String>) { url ->
                             AsyncImage(
-                                model = "https://ipsf.net/wp-content/uploads/2021/12/dummy-image-square.webp",//url,
+                                model = url,
+                                contentDescription = null,
+                                contentScale = ContentScale.FillHeight,
+                                modifier = Modifier.fillMaxHeight(0.5f)
+                            )
+                        }
+                    }
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        FloatingActionButton(
+                            modifier = Modifier.padding(24.dp),
+                            onClick = {
+                                if (editor) {
+                                    if (!descError && !urlError) {
+                                        editUrl.split("\n")
+                                            .map { it.trim() }
+                                            .let { MainUI.map.updateEntry(label, editDesc, it) }
+
+                                        editor = false
+                                    }
+                                } else {
+                                    editor = true
+                                }
+                            }) {
+                            Icon(if (editor) Icons.Filled.Done else Icons.Filled.Edit, null)
+                        }
+
+                        FloatingActionButton(
+                            modifier = Modifier.padding(24.dp),
+                            onClick = {
+                                navController.navigate(Navigator.ARNav.route)
+                                scope.launch {
+                                    snackbarHostState.showSnackbar("[INFO] Routing to $label")
+                                }
+                            }
+                        ) {
+                            Icon(Icons.Filled.Send, null)
+                        }
+                    }
+                }
+
+                LazyColumn(
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    item{ Text(label, fontSize = 24.sp)}
+                    item { (MainUI.map.mapNodes[label]?.get("longDesc") as? String)?.let { Text(it) } }
+
+                    if (editor) {
+                        item {
+                            TextField(
+                                value = editDesc,
+                                onValueChange = {
+                                    descError = it.isEmpty()
+                                    editDesc = it
+                                },
+                                label = { Text("Description") },
+                                singleLine = false,
+                                isError = descError,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+
+                        item {
+                            TextField(
+                                value = editUrl,
+                                onValueChange = {
+                                    urlError = it.isEmpty()
+                                    editUrl = it
+                                },
+                                label = { Text("Image URLs (newline-separated)") },
+                                singleLine = false,
+                                isError = urlError,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                    }
+                }
+            } else {
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.BottomEnd
+                ) {
+                    LazyRow {
+                        items(MainUI.map.mapNodes[label]?.get("imagesUrl") as List<String>) { url ->
+                            AsyncImage(
+                                model = url,
                                 contentDescription = null,
                                 contentScale = ContentScale.FillHeight,
                                 modifier = Modifier.fillMaxHeight(0.5f)
@@ -427,22 +453,21 @@ fun EntryPage(
                                 snackbarHostState.showSnackbar("[INFO] Routing to $label")
                             }
                         }
-                    ){
+                    ) {
                         Icon(Icons.Filled.Send, null)
                     }
                 }
 
-                Column(
+                LazyColumn(
                     horizontalAlignment = Alignment.Start,
                     verticalArrangement = Arrangement.spacedBy(16.dp),
                     modifier = Modifier.padding(16.dp)
-                ){
-                    Text("A123" /*label*/, fontSize = 24.sp)
-                    (MainUI.map.mapNodes[label]?.get("longDesc") as? String)?.let { Text(it) }
-                    }
+                ) {
+                    item { Text(label, fontSize = 24.sp) }
+                    item { (MainUI.map.mapNodes[label]?.get("longDesc") as? String)?.let { Text(it) } }
+                }
             }
-
-//        }
+        }
 
         SnackbarHost(
             hostState = snackbarHostState,
